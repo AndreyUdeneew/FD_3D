6172f3481e6deae1e7f4e22d045cc83df66410b7
Andrej <andrey.udeneew@mail.ru>
2025-09-04 01:10:56 +0300

    v0.5 seems 2B ready

--- FD_3D/main.py
+++ FD_3D/main.py
@@ -11,52 +11,68 @@ import matplotlib.patches as patches
 
 def openRed():
     global fileNameRed
-    fileNameRed = askopenfilenames(parent=window)
-    imRed = cv2.imdecode(np.fromfile(fileNameRed, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-    imRed = cv2.cvtColor(imRed, cv2.COLOR_BGR2RGB)
-    imRed = imRed[:, :, 1]  # 2 for Nikita, 0 for Inessa
+    fileNameRed = askopenfilename(parent=window)
+    print(fileNameRed)
+    text1.insert(INSERT, fileNameRed)
     return
 
 def openGLED():
     global fileNameGLED
-    fileNameGLED = askopenfilenames(parent=window)
-    imGLED = cv2.imdecode(np.fromfile(fileNameGLED, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-    imGLED = cv2.cvtColor(imGLED, cv2.COLOR_BGR2RGB)
-    imGLED = imGLED[:, :, 1]  # 2 for Nikita, 0 for Inessa
+    fileNameGLED = askopenfilename(parent=window)
+    print(fileNameGLED)
+    text2.insert(INSERT, fileNameGLED)
     return
 
 def openRLED():
     global fileNameRLED
-    fileNameRLED = askopenfilenames(parent=window)
-    imRLED = cv2.imdecode(np.fromfile(fileNameRLED, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-    imRLED = cv2.cvtColor(imRLED, cv2.COLOR_BGR2RGB)
-    imRLED = imRLED[:, :, 1]  # 2 for Nikita, 0 for Inessa
+    fileNameRLED = askopenfilename(parent=window)
+    print(fileNameRLED)
+    text3.insert(INSERT, fileNameRLED)
     return
 
 def processing():
-    global imRed, imGLED, imRLED
+    global imRed, imGLED, imRLED, fileNameRed, fileNameGLED, fileNameRLED
     fig    = plt.figure()
     ax     = fig.add_subplot(projection='3d')
 
-    x      = np.linspace(0, 1, 100)
-    X, Y   = np.meshgrid(x, x)
-    levels = np.linspace(-0.1, 0.4, 100)  #(z_min,z_max,number of contour),
+    x      = np.linspace(1, 1440, 1440)
+    y = np.linspace(1, 1080, 1080)
+    X, Y   = np.meshgrid(x, y)
+    levels = np.linspace(0, 255, 256)  #(z_min,z_max,number of contour),
 
     a=0
     b=1
     c=2
-    Z1 = a+.1*np.sin(2*X)*np.sin(4*Y)
-    Z2 = b+.1*np.sin(3*X)*np.sin(4*Y)
-    Z3 = c+.1*np.sin(4*X)*np.sin(5*Y)
+    # Z1 = a+.1*np.sin(2*X)*np.sin(4*Y)
+    # Z2 = b+.1*np.sin(3*X)*np.sin(4*Y)
+    # Z3 = c+.1*np.sin(4*X)*np.sin(5*Y)
 
-    plt.contourf(X, Y,Z1, levels=a+levels,cmap=plt.get_cmap('rainbow'))
-    plt.contourf(X, Y,Z2, levels=b+levels,cmap=plt.get_cmap('rainbow'))
-    plt.contourf(X, Y,Z3, levels=c+levels,cmap=plt.get_cmap('rainbow'))
+    # plt.contourf(X, Y,Z1, levels=a+levels,cmap=plt.get_cmap('rainbow'))
+    # plt.contourf(X, Y,Z2, levels=b+levels,cmap=plt.get_cmap('rainbow'))
+    # plt.contourf(X, Y,Z3, levels=c+levels,cmap=plt.get_cmap('rainbow'))
+    imRed = cv2.imdecode(np.fromfile(fileNameRed, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+    imRed = cv2.cvtColor(imRed, cv2.COLOR_BGR2RGB)
+    imRed = imRed[:, :, 1]  # 2 for Nikita, 0 for Inessa
+    # cv2.imshow('Red', imRed)
 
-    ax.set_xlim3d(0, 1)
-    ax.set_ylim3d(0, 1)
-    ax.set_zlim3d(0, 2)
+    imGLED = cv2.imdecode(np.fromfile(fileNameGLED, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+    imGLED = cv2.cvtColor(imGLED, cv2.COLOR_BGR2RGB)
+    imGLED = imGLED[:, :, 1]  # 2 for Nikita, 0 for Inessa
+    # cv2.imshow('GLED', imGLED)
 
+    imRLED = cv2.imdecode(np.fromfile(fileNameRLED, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+    imRLED = cv2.cvtColor(imRLED, cv2.COLOR_BGR2RGB)
+    imRLED = imRLED[:, :, 1]  # 2 for Nikita, 0 for Inessa
+    # cv2.imshow('RLED', imRLED)
+
+    plt.contourf(X, Y, imRed, offset=0, cmap=plt.get_cmap('rainbow'))
+    plt.contourf(X, Y, imGLED,offset=512, cmap=plt.get_cmap('rainbow'))
+    plt.contourf(X, Y, imRLED,offset=1024,  cmap=plt.get_cmap('rainbow'))
+
+    ax.set_xlim3d(0, 1440)
+    ax.set_ylim3d(0, 1080)
+    ax.set_zlim3d(0, 1500)
+    text4.insert(INSERT, "Ready")
     plt.show()
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -67,11 +83,11 @@ if __name__ == '__main__':
     window = Tk()
     window.geometry('1150x650')
     window.title("Осознанная тераностика")
-    text1 = Text(width=70, height=1)  # image
+    text1 = Text(width=100, height=1)  # image
     text1.grid(column=1, row=0, sticky=W)
-    text2 = Text(width=70, height=1)  # image
+    text2 = Text(width=100, height=1)  # image
     text2.grid(column=1, row=1, sticky=W)
-    text3 = Text(width=70, height=1)  # image
+    text3 = Text(width=100, height=1)  # image
     text3.grid(column=1, row=2, sticky=W)
     text4 = Text(width=15, height=1)  # image
     text4.grid(column=1, row=3, sticky=W)
